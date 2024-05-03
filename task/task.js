4ac778f223a698912077200d81b6ae1cdbbd9436
seokwns <seokjun0915@icloud.com>
2024-05-03 15:56:46 +0900

    feat: add hardhat task
    - staking module, node operator summary 조회 task 추가

--- task/task.js
+++ task/task.js
@@ -1,14 +1,52 @@
+const fs = require("fs");
+const path = require("path");
 const { task } = require("hardhat/config");
+const GAS_INFO = {
+  gasLimit: 1000000,
+  gasPrice: 100000,
+};
+
+task("staking-module-summary", "Get staking module summary")
+  .addParam("module", "The staking module id (>= 1)")
+  .setAction(async (taskArgs, { ethers }) => {
+    const getContracts = require("../scripts/interact/loader");
+    const loader = await getContracts();
+    const STAKING_MODULE_ID = taskArgs.id > 1? taskArgs.id : 1;
+
+    console.log();
+    console.log("- staking module id:", STAKING_MODULE_ID);
+
+    const data = await loader.StakingRouter.contract.getStakingModuleSummary(STAKING_MODULE_ID, GAS_INFO);
+    console.log();
+    console.log("- Staking Module Summary:", data);
+  });
+
+task("node-operator-summary", "Get node operator summary")
+  .addParam("module", "The staking module id (>= 1)")
+  .addParam("operator", "The node operator id (>= 0)")
+  .setAction(async (taskArgs, { ethers }) => {
+    const getContracts = require("../scripts/interact/loader");
+    const loader = await getContracts();
+    const STAKING_MODULE_ID = taskArgs["module-id"] > 1? taskArgs["module-id"] : 1;
+    const NODE_OPERATOR_ID = taskArgs["operator-id"] > 0? taskArgs["operator-id"] : 0;
+
+    console.log();
+    console.log("- staking module id:", STAKING_MODULE_ID);
+    console.log("- node operator id:", NODE_OPERATOR_ID);
+
+    const data = await loader.StakingRouter.contract.getNodeOperatorSummary(STAKING_MODULE_ID, NODE_OPERATOR_ID, GAS_INFO);
+    console.log();
+    console.log("- Node Operator Summary:", data);
+  });
 
 task("total-ace", "Get total ACE balance")
   .setAction(async (taskArgs, { ethers }) => {
     const getContracts = require("../scripts/interact/loader");
-
     const loader = await getContracts();
     const catalist = loader.Catalist.contract;
 
-    const data = await catalist.getTotalPooledAce();
-    const decimals = await catalist.decimals();
+    const data = await catalist.getTotalPooledAce(GAS_INFO);
+    const decimals = await catalist.decimals(GAS_INFO);
     const pooledAce = data / (10 ** +decimals);
     console.log();
     console.log("- Total Polled ACE:", pooledAce.toFixed(+decimals));
@@ -19,44 +57,66 @@ task("add-operator", "Add validator operator")
   .addParam("address", "The operator reward address")
   .setAction(async (taskArgs, { ethers }) => {
     const getContracts = require("../scripts/interact/loader");
-
-    const GAS_INFO = {
-      gasLimit: 1000000,
-      gasPrice: 100000,
-    };
-
+    const loader = await getContracts();
     const OPERATOR_NAME = taskArgs.name;
     const OPERATOR_ADDRESS = taskArgs.address;
-
-    const loader = await getContracts();
 
     console.log();
     console.log("- name:", OPERATOR_NAME);
     console.log("- address:", OPERATOR_ADDRESS);
 
-    await loader.NodeOperatorsRegistry.contract.addOperator(
+    const tx = await loader.NodeOperatorsRegistry.contract.addOperator(
       OPERATOR_NAME,
       OPERATOR_ADDRESS,
       GAS_INFO
     );
 
     console.log();
+    console.log("- transaction hash:", tx.hash);
+
+    console.log();
     console.log("Complete.");
   });
 
-task("add-key", "Add validator singing keys")
+task("remove-keys", "Remove validator singing keys")
+  .addParam("operator", "The operator id")
+  .addParam("index", "The key index")
+  .addParam("count", "The key count")
+  .setAction(async (taskArgs, { ethers }) => {
+    const getContracts = require("../scripts/interact/loader");
+    const loader = await getContracts();
+    const NODE_OPERATOR_ID = taskArgs.operator;
+    const KEY_INDEX = taskArgs.index;
+    const KEY_COUNT = taskArgs.count;
+
+    console.log();
+    console.log("- operator:", NODE_OPERATOR_ID);
+    console.log("- index:", KEY_INDEX);
+    console.log("- count:", KEY_COUNT);
+
+    const tx = await loader.NodeOperatorsRegistry.contract.removeSigningKeys(
+      NODE_OPERATOR_ID,
+      KEY_INDEX,
+      KEY_COUNT,
+      GAS_INFO
+    );
+
+    console.log();
+    console.log("- transaction hash:", tx.hash);
+
+    console.log();
+    console.log("Complete.");
+  });
+
+task("add-keys", "Add validator singing keys")
   .addParam("operator", "The operator id")
   .addParam("file", "Operator signing keys file path")
   .setAction(async (taskArgs, { ethers }) => {
-    const fs = require("fs");
-    const path = require("path");
-    const getContracts = require("../scripts/interact/loader");
     const { hexConcat } = require('../scripts/interact/utils')
-
+    const getContracts = require("../scripts/interact/loader");
+    const loader = await getContracts();
     const NODE_OPERATOR_ID = taskArgs.operator;
     const DEPOSIT_DATA_FILE = path.resolve(__dirname, "../" + taskArgs.file);
-
-    const loader = await getContracts();
 
     console.log()
     console.log("- operator:", NODE_OPERATOR_ID);
@@ -71,13 +131,16 @@ task("add-key", "Add validator singing keys")
     console.log("Add signing keys...");
     console.log("- count:", KEY_COUNT);
 
-    await loader.NodeOperatorsRegistry.contract.addSigningKeys(
+    const tx = await loader.NodeOperatorsRegistry.contract.addSigningKeys(
       NODE_OPERATOR_ID,
       KEY_COUNT,
       hexConcat(...pubkeys),
       hexConcat(...signatures),
       GAS_INFO
     );
+
+    console.log();
+    console.log("- transaction hash:", tx.hash);
 
     console.log();
     console.log("Complete.");
@@ -89,17 +152,11 @@ task("get-keys", "Get validator singing keys")
   .addParam("end", "The end index")
   .setAction(async (taskArgs, { ethers }) => {
     const getContracts = require("../scripts/interact/loader");
-
-    const GAS_INFO = {
-      gasLimit: 1000000,
-      gasPrice: 100000,
-    };
-
+    const loader = await getContracts();
     const NODE_OPERATOR_ID = taskArgs.operator;
     const START = taskArgs.start;
     const END = taskArgs.end;
 
-    const loader = await getContracts();
 
     console.log();
     console.log("- operator:", NODE_OPERATOR_ID);
@@ -120,10 +177,8 @@ task("event-log", "Interact with mainnet event logs")
   .addParam("event", "The event to trace")
   .addParam("contract", "The contract to trace")
   .setAction(async (taskArgs, { ethers }) => {
-    const fs = require("fs");
-    const path = require("path");
     const getContracts = require("../scripts/interact/loader");
-
+    const loader = await getContracts();
     const EVENT = taskArgs.event;
     const contract = taskArgs.contract;
 
@@ -131,8 +186,7 @@ task("event-log", "Interact with mainnet event logs")
     console.log('- Event:', EVENT)
     console.log('- Contract:', contract)
     console.log()
-
-    const loader = await getContracts();
+    
     const CONTRACT = loader[contract].contract;
     const CONTRACT_ADDRESS = loader[contract].address;
     const CONTRACT_NAME = loader[contract].name;
