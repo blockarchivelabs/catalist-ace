16a5de7399d619f096395d0afab500d5c70a6b07
realmankwon <realman0214@gmail.com>
2024-06-07 15:17:57 +0900

    feat: CHURN_VALIDATORS_PER_DAY_LIMIT_MANAGER_ROLE task add

--- scripts/interact/loader.js
+++ scripts/interact/loader.js
@@ -10,52 +10,94 @@ async function getContracts() {
     DepositContract: {
       name: 'DepositContract',
       address: addresses.chainSpec.depositContract,
-      contract: await ethers.getContractAt('DepositContract', addresses.chainSpec.depositContract)
+      contract: await ethers.getContractAt(
+        'DepositContract',
+        addresses.chainSpec.depositContract,
+      ),
     },
     Catalist: {
       name: 'Catalist',
       address: addresses['app:catalist'].proxy.address,
-      contract: await ethers.getContractAt('Catalist', addresses['app:catalist'].proxy.address),
-      proxy: await ethers.getContractAt('AppProxyUpgradeable', addresses['app:catalist'].proxy.address)
+      contract: await ethers.getContractAt(
+        'Catalist',
+        addresses['app:catalist'].proxy.address,
+      ),
+      proxy: await ethers.getContractAt(
+        'AppProxyUpgradeable',
+        addresses['app:catalist'].proxy.address,
+      ),
     },
     HashConsensusForAccountingOracle: {
       name: 'HashConsensus',
       address: addresses.hashConsensusForAccountingOracle.address,
-      contract: await ethers.getContractAt('HashConsensus', addresses.hashConsensusForAccountingOracle.address)
+      contract: await ethers.getContractAt(
+        'HashConsensus',
+        addresses.hashConsensusForAccountingOracle.address,
+      ),
     },
     HashConsensusForValidatorsExitBusOracle: {
       name: 'HashConsensus',
       address: addresses.hashConsensusForValidatorsExitBusOracle.address,
-      contract: await ethers.getContractAt('HashConsensus', addresses.hashConsensusForValidatorsExitBusOracle.address)
+      contract: await ethers.getContractAt(
+        'HashConsensus',
+        addresses.hashConsensusForValidatorsExitBusOracle.address,
+      ),
     },
     StakingRouter: {
       name: 'StakingRouter',
       address: addresses.stakingRouter.proxy.address,
-      contract: await ethers.getContractAt('StakingRouter', addresses.stakingRouter.proxy.address),
-      proxy: await ethers.getContractAt('OssifiableProxy', addresses.stakingRouter.proxy.address)
+      contract: await ethers.getContractAt(
+        'StakingRouter',
+        addresses.stakingRouter.proxy.address,
+      ),
+      proxy: await ethers.getContractAt(
+        'OssifiableProxy',
+        addresses.stakingRouter.proxy.address,
+      ),
     },
     AccountingOracle: {
       name: 'AccountingOracle',
       address: addresses.accountingOracle.proxy.address,
-      contract: await ethers.getContractAt('AccountingOracle', addresses.accountingOracle.proxy.address),
-      proxy: await ethers.getContractAt('OssifiableProxy', addresses.accountingOracle.proxy.address)
+      contract: await ethers.getContractAt(
+        'AccountingOracle',
+        addresses.accountingOracle.proxy.address,
+      ),
+      proxy: await ethers.getContractAt(
+        'OssifiableProxy',
+        addresses.accountingOracle.proxy.address,
+      ),
     },
     WithdrawalQueueERC721: {
       name: 'WithdrawalQueueERC721',
       address: addresses.withdrawalQueueERC721.proxy.address,
-      contract: await ethers.getContractAt('WithdrawalQueueERC721', addresses.withdrawalQueueERC721.proxy.address),
-      proxy: await ethers.getContractAt('OssifiableProxy', addresses.withdrawalQueueERC721.proxy.address)
+      contract: await ethers.getContractAt(
+        'WithdrawalQueueERC721',
+        addresses.withdrawalQueueERC721.proxy.address,
+      ),
+      proxy: await ethers.getContractAt(
+        'OssifiableProxy',
+        addresses.withdrawalQueueERC721.proxy.address,
+      ),
     },
     NodeOperatorsRegistry: {
       name: 'NodeOperatorsRegistry',
       address: addresses['app:node-operators-registry'].proxy.address,
-      contract: await ethers.getContractAt('NodeOperatorsRegistry', addresses['app:node-operators-registry'].proxy.address),
-      proxy: await ethers.getContractAt('AppProxyUpgradeable', addresses['app:node-operators-registry'].proxy.address)
+      contract: await ethers.getContractAt(
+        'NodeOperatorsRegistry',
+        addresses['app:node-operators-registry'].proxy.address,
+      ),
+      proxy: await ethers.getContractAt(
+        'AppProxyUpgradeable',
+        addresses['app:node-operators-registry'].proxy.address,
+      ),
     },
     DepositSecurityModule: {
       name: 'DepositSecurityModule',
       address: addresses.depositSecurityModule.address,
-      contract: await ethers.getContractAt('DepositSecurityModule', addresses.depositSecurityModule.address)
+      contract: await ethers.getContractAt(
+        'DepositSecurityModule',
+        addresses.depositSecurityModule.address,
+      ),
     },
     Burner: {
       name: 'Burner',
@@ -65,28 +107,60 @@ async function getContracts() {
     CatalistLocator: {
       name: 'CatalistLocator',
       address: addresses.catalistLocator.proxy.address,
-      contract: await ethers.getContractAt('CatalistLocator', addresses.catalistLocator.proxy.address),
-      proxy: await ethers.getContractAt('OssifiableProxy', addresses.catalistLocator.proxy.address)
+      contract: await ethers.getContractAt(
+        'CatalistLocator',
+        addresses.catalistLocator.proxy.address,
+      ),
+      proxy: await ethers.getContractAt(
+        'OssifiableProxy',
+        addresses.catalistLocator.proxy.address,
+      ),
     },
     ValidatorsExitBusOracle: {
       name: 'ValidatorsExitBusOracle',
       address: addresses.validatorsExitBusOracle.proxy.address,
-      contract: await ethers.getContractAt('ValidatorsExitBusOracle', addresses.validatorsExitBusOracle.proxy.address),
-      proxy: await ethers.getContractAt('OssifiableProxy', addresses.validatorsExitBusOracle.proxy.address)
+      contract: await ethers.getContractAt(
+        'ValidatorsExitBusOracle',
+        addresses.validatorsExitBusOracle.proxy.address,
+      ),
+      proxy: await ethers.getContractAt(
+        'OssifiableProxy',
+        addresses.validatorsExitBusOracle.proxy.address,
+      ),
     },
     AragonKernel: {
       name: 'AragonKernel',
       address: addresses['aragon-kernel'].proxy.address,
-      contract: await ethers.getContractAt('Kernel', addresses['aragon-kernel'].proxy.address),
-      proxy: await ethers.getContractAt('KernelProxy', addresses['aragon-kernel'].proxy.address)
+      contract: await ethers.getContractAt(
+        'Kernel',
+        addresses['aragon-kernel'].proxy.address,
+      ),
+      proxy: await ethers.getContractAt(
+        'KernelProxy',
+        addresses['aragon-kernel'].proxy.address,
+      ),
     },
     AragonAcl: {
       name: 'AragonAcl',
       address: addresses['aragon-acl'].proxy.address,
-      contract: await ethers.getContractAt('ACL', addresses['aragon-acl'].proxy.address),
-      proxy: await ethers.getContractAt('AppProxyUpgradeable', addresses['aragon-acl'].proxy.address)
-    }
-  };  
+      contract: await ethers.getContractAt(
+        'ACL',
+        addresses['aragon-acl'].proxy.address,
+      ),
+      proxy: await ethers.getContractAt(
+        'AppProxyUpgradeable',
+        addresses['aragon-acl'].proxy.address,
+      ),
+    },
+    OracleReportSanityChecker: {
+      name: 'OracleReportSanityChecker',
+      address: addresses.oracleReportSanityChecker.address,
+      contract: await ethers.getContractAt(
+        'OracleReportSanityChecker',
+        addresses.oracleReportSanityChecker.address,
+      ),
+    },
+  };
 
   return contracts;
 }
